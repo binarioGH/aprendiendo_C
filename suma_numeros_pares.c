a5e36a7a0f13faf07c221a0c60f86b8a2bbe5f22
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-04-25 20:42:49 -0500

    programa de practica para aprender ciclos for

---
+++ suma_numeros_pares.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// https://www.youtube.com/watch?v=k0sjuSzehm8
+int main(){
+	int i, suma = 0;
+	for(i=1; i<=100; i += 2){
+		suma += i;
+		
+	}
+	printf("la suma da en total: %i", suma);
+	
+	return 0;
+}
+
