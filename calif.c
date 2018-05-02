a148f092d664f587acc55b4b44992b3eec9a42ff
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-05-02 12:42:58 -0500

    programa simple que me dijeron que hiciera en la escuela

--- calif.c
+++ calif.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-void main(){
-    int calif1, calif2, calif3, calif4, total, total2;
-    printf(" introduzca el valor de la primera nota: ");
-    scanf("%i", &calif1);
-    printf("\n introduzca el valor de la segunda: ");
-    scanf("%i", &calif2);
-    printf("\n introduzca el valor de la tercera: ");
-    scanf("%i", &calif3);
-    printf("\n introduzca el valor de la cuarta: ");
-    scanf("%i", &calif4);
-    total = calif1 + calif2 + calif3 + calif4;
-    total2 = total / 4;
-    printf("el promedio es: %i",total2);
+
+int main(){
+	float total, examen, proyecto, trabajo;
+	printf("cuanto sacaste en examen? ");
+	scanf("%f", &examen);
+	printf("cuanto sacaste en el proyecto? ");
+	scanf("%f", &proyecto);
+	printf("cuanto sacaste en trabajos? ");
+	scanf("%f", &trabajo);
+	examen = examen * 0.4;
+	proyecto = proyecto * 0.4;
+	trabajo = trabajo * 0.2;
+	total = examen + trabajo + proyecto;
+	if (total >= 6.5 && total <= 6.9){
+	total = 7;
+	}
+	else if(total >= 7.5 && total <= 7.9){
+		total = 8;
+	}
+	else if(total >= 8.5 && total <= 8.9){
+		total = 9;
+	}
+	else if(total >= 9.5 && total <= 9.9){
+		total = 10;
+	}
+	printf("tu promedio es: %.1f \n", total);
+	if (total < 5.9){
+		printf("reprobaste, debes de estudiar para la proxima");
+	}
+	else if(total < 7){
+		printf("tu promedio es suficiente para pasar, pero puedes mejorar");
+	}
+	else if(total < 9 && total > 7){
+		printf("tu promedio no esta nada mal, sigue estudiando");
+	}
+	else if(total == 10){
+		printf("tu promedio es perfecto");
+	}
+	return 0;
 }
