2c2ab058298425b644b3feaa0b5a4edb155ae5b6
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-08-25 16:11:39 -0500

    arreglos

---
+++ codigo_facilito/12.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+	int arreglo[3] = {1,2,3};
+	printf("%i", arreglo[0]);
+	return 0;
+}
---
+++ codigo_facilito/12v2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+	int multi[2][3] = {{1,2,3},{4,5,6}};
+	printf("%i", multi[1][2]);
+}
