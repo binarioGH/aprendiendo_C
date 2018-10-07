ba04d00afa7f45f44da941c398572194940d7936
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-10-07 17:07:07 -0500

    errores solucionados

--- kevin/7.c
+++ kevin/7.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
-	int dinero = rand()%250+100, des;
-	char productos[5][9] = {"Arroz", "Jamon", "Atun", "Platano", "Chocolate"};
+	srand (time(NULL));
+	int dinero = rand()%(301-100);
+	int des;
+	char productos[5][10] = {"Arroz", "Jamon", "Atun", "Platano", "Chocolate"};
 	int costo[5] = {5, 10, 20, 15, 12}, i;
 
 	do{
-		fflush(stdin);
 		system("cls");
 		printf("Fondo: %i", dinero);
 		for(i=0;i < 5;i++){
@@ -15,13 +17,11 @@ int main(){
 		}
 		printf("\n100. Salir.\n");
 		printf("\nQue producto desea comprar?: ");scanf("%i", &des);
-		if(des < 5){continue;}
+		if(des >= 5){continue;}
 		else{
 			dinero -= costo[des];
 		}
 
-
-		
 	}while(des != 100);
 
 	return 0;
