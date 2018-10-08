79d04140f03a1e93329aa23732d689f23e57b7dc
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-10-07 19:40:08 -0500

    bugs arreglados

--- kevin/5.c
+++ kevin/5.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 struct alumno{
+	char name[20];
 	int edad;
 	int promedio;
-
-}alumnos[7];
-int main(){
+}alumnos[50];
+	int main(){
+	srand (time(NULL));
 	int cantidad, i, n;
 	char names[7][10] = {"Juan", "Max", "Julia", "Pablo", "Alejandra", "Regina", "Angel"};
+	char lstnames[5][10] = {"Perez", "Gonzales", "Martinez", "Rodriguez", "Contreras"};
 	printf("Ingrese la cantidad de alumnos: ");scanf("%i", &cantidad);
-	for(i=0;i<cantidad-1;i++){
-		alumnos[i].edad = rand() % 12 + 18;
-		alumnos[i].promedio = rand() % 10;
-		printf("----------\nNombre: %s\nEdad: %i\nPromedio: %i\n", names[i], alumnos[i].edad,alumnos[i].promedio); 
+	for(i=0;i<cantidad;i++){
+		snprintf( &( alumnos[i].name ), sizeof( alumnos[i].name ), "Nombre: %s %s",names[rand( ) % 8], lstnames[rand( ) % 6] );
+		alumnos[i].edad = rand() % 12 + 19;
+		alumnos[i].promedio = rand() % 11;
+		printf("\n----------\nNombre: %s\nEdad: %i\nPromedio: %i\n",alumnos[i].name, alumnos[i].edad,alumnos[i].promedio); 
 	}
 	return 0;
 }
